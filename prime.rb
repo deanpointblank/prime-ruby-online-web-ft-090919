d2dbc7b787e8a162e7373552cfaace5ded6edbfa
Dean L Betty <deanlloyd64@gmail.com>
2019-09-12 16:57:41 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,12 @@
 # Add  code here!
 def prime?(num)
-  if num <= 0
+  if num < 2
     false
+  elsif num == 2 || num == 3
   else
     range = (2..num).to_a
-    range.any? do |int|
-      num % int != 0
+    range.none? do |int|
+      num % int == 0
     end
   end
 end
